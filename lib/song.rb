5d64784d7b570c0ebbf30700a40ffff20790a4cf
Adam G Horn <hornadam8@gmail.com>
2020-01-31 03:11:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,43 @@
 class Song
+  
   attr_accessor :name, :artist_name
+ 
   @@all = []
-
+  
+  def self.create
+    @@all << self.new
+    @@all[-1]
+  end
+  
+  def self.new_by_name(title)
+    @@all << self.new
+    @@all[-1].name = title
+    @@all[-1]
+  end
+  
+  def self.create_by_name(title)
+    @@all << self.new
+    @@all[-1].name = title
+    @@all[-1]
+  end
+  
+  def self.find_by_name(title)
+    @@all.find{|song| song.name == title} 
+  end
+  
+  def self.find_or_create_by_name(title)
+   self.find_by_name(title) || self.create_by_name(title)
+  end
+  
+  def self.alphabetical
+    @@all.sort_by{|x| x.name}
+  end
+  
+  def self.new_from_filename(file)
+    word = Song.new
+    
+  end
+  
   def self.all
     @@all
   end
